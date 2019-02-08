37f20638357b396659b597741aeb6f8c290c9740
G <gabeanthonynorman@outlook.com>
2019-02-08 16:53:43 -0500

    Added mongoose support

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
 
-    "URi":"mongodb://localhost/test"
+    //"URi":"mongodb://localhost/test"
+    "URi":"mongodb://<dbuser>:<dbpassword>@ds050739.mlab.com:50739/comp308-w2019"
+
+    
 }
